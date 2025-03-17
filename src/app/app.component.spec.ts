78f4d58b5aff1da048b8414f519bf401914b78f7
yuvrajdas <yuvrajdas1531@gmail.com>
2025-03-17 22:12:50 +0530

    nitial assignmen setup

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,7 +1,7 @@
 import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 
-describe('AppComponent', () => {
+describe('Test Driven Development', () => {
   beforeEach(() => TestBed.configureTestingModule({
     declarations: [AppComponent]
   }));
@@ -12,16 +12,5 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'incubyte_tigi_hr__assignment'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('incubyte_tigi_hr__assignment');
-  });
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('incubyte_tigi_hr__assignment app is running!');
-  });
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,9 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  template: `<h2>Test Driven Development</h2>`,
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'incubyte_tigi_hr__assignment';
+
 }
